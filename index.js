f71c9238c16494af40aea9149f9529fcb8fcc8ca
visakhcv <visakhvisa12345@gmail.com>
2023-05-14 20:04:19 +0530

    foodx-backend v1

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const router=require('./routes/router')
 const jwt=require('jsonwebtoken')
 require('./db/connection')
 const server=express()
-const port=3000
+const port=3000 || process.env.PORT
 // app specific middleware
 
 
